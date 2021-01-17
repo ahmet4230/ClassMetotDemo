252a54aaf3dec0fa7324cb8ec9546c8d8fa29be6
example <ahmet_batman123@hotmail.com>
2021-01-17 18:11:33 +0300

    project pertama

---
+++ MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi: " + musteri.MusteriAd + " " + musteri.MusteriSoyad + " Id no: " + musteri.MusterId);
+        }
+        
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi: " + musteri.MusteriAd + " " + musteri.MusteriSoyad + " Id no: " + musteri.MusterId);
+        }
+
+        public void MusteriListele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("MÜŞTERİ LİSTESİ " + musteri.MusteriAd + " " + musteri.MusteriSoyad + " Id no: " + musteri.MusterId);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,46 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.MusterId = "0001";
+            musteri1.MusteriAd = "Ahmet";
+            musteri1.MusteriSoyad = "BATMAN";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusterId = "0002";
+            musteri2.MusteriAd = "Samet";
+            musteri2.MusteriSoyad = "ATEŞ";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.MusterId = ("00003");
+            musteri3.MusteriAd = ("Erdoğan Kamil");
+            musteri3.MusteriSoyad = ("GÜLEÇ");
+
+            Musteri musteri4 = new Musteri();
+            musteri4.MusterId = ("0004");
+            musteri4.MusteriAd = ("Ali Ulvi");
+            musteri4.MusteriSoyad = ("BATMAN");
+
+
+            MusteriManager musteri = new MusteriManager();
+            
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 , musteri4};
+
+            musteri.MusteriEkle(musteri4);
+            musteri.MusteriEkle(musteri3);
+            musteri.MusteriSil(musteri2);
+            musteri.MusteriListele(musteriler);
+            musteri.MusteriEkle(musteri2);
+
+            
+           
+            
+           
+
+
+
+           
         }
     }
 }
